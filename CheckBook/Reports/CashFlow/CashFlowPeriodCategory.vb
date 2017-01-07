28d5488bd8ad973f2bee38660877cad2ce8aca03
Timothy Yaukey <yaukey@willowsoft.com>
2017-01-07 12:14:38 -0800

    Remove unused classes for the umimplemented cash flow report.

--- CheckBook/Reports/CashFlow/CashFlowPeriodCategory.vb
+++
@@ -1,9 +0,0 @@
-Option Strict Off
-Option Explicit On
-'Public Class CashFlowPeriodCategory
-'    '2345667890123456789012345678901234567890123456789012345678901234567890123456789012345
-
-'    'Splits without budgets in one CashFlowPeriod and one Register.
-
-'    Public colSplits As Collection 'Splits w/o budgets
-'End Class
