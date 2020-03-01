7cf4761d32f28afe79e2908b6f60152f10f32a18
Timothy Yaukey <yaukey@willowsoft.com>
2020-02-29 20:45:47 -0800

    Update version number of CheckBook and CheckBookLib to 2.7.0.

--- CheckBook/AssemblyInfo.vb
+++ CheckBook/AssemblyInfo.vb
@@ -28,9 +28,9 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.6.0")>
+<Assembly: AssemblyVersion("2.7.0")>
 
 
 
 <Assembly: ComVisibleAttribute(False)>
-<Assembly: AssemblyFileVersion("2.6.0")>
+<Assembly: AssemblyFileVersion("2.7.0")>
