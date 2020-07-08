2ff4e1d8ab3ef954dcd09c29745a744994fd24d4
Timothy Yaukey <yaukey@willowsoft.com>
2020-07-07 18:43:18 -0700

    Switch to using TamperProofConsole.

--- MainLicenseConsole/Program.cs
+++ MainLicenseConsole/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LicenseConsole
 {
@@ -10,40 +6,10 @@ namespace LicenseConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Create License File For Willow Creek Checkbook");
-            Console.Write("Name of entity to license (e.g. \"John Smith\" or \"Chess Club\"): ");
-            string licensedTo = Console.ReadLine();
-            Console.Write("Expiration date (\"mm/dd/yyyy\", blank if never expires): ");
-            string expDateText = Console.ReadLine();
-            DateTime? expirationDate;
-            if (string.IsNullOrEmpty(expDateText))
-                expirationDate = null;
-            else
-            {
-                DateTime expDateTemp;
-                if (!DateTime.TryParseExact(expDateText, "MM/dd/yyyy", null, System.Globalization.DateTimeStyles.None, out expDateTemp))
-                {
-                    Console.WriteLine("Invalid expiration date.");
-                    return;
-                }
-                expirationDate = expDateTemp;
-            }
-            Console.Write("Email address of this user: ");
-            string emailAddress = Console.ReadLine();
-            Console.Write("License serial number (anything will work): ");
-            string serialNumber = Console.ReadLine();
-            if (string.IsNullOrEmpty(licensedTo) || string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(serialNumber))
-            {
-                Console.WriteLine("Missing data.");
-                return;
-            }
-            Dictionary<string, string> values = Willowsoft.TamperProofData.StandardLicenseBuilder.Build(licensedTo, expirationDate, emailAddress, serialNumber, 1);
-            const string userLicenseFileName = "User.lic";
-            using (System.IO.Stream output = new System.IO.FileStream(userLicenseFileName, System.IO.FileMode.Create))
-            {
-                Willowsoft.TamperProofData.LicenseWriter.Write(values, new Willowsoft.CheckBook.LicenseGenerator.MainLicenseSigner(), output);
-                Console.Write("User license written to " + userLicenseFileName);
-            }
+            new Willowsoft.TamperProofConsole.StandardLicenseConsole
+                <Willowsoft.CheckBook.LicenseGenerator.MainLicenseSigner>
+                ("Willow Creek Checkbook2", "User.lic", 1)
+                .Run();
         }
     }
 }
