1527a3b21dc137365586531c6c065849abc39949
Timothy Yaukey <yaukey@willowsoft.com>
2020-09-28 20:11:16 -0700

    Fix RegisterSaver and LiveBudgetListForm to use Register.colAllTrx.

--- CheckBook/LiveBudgetListForm.vb
+++ CheckBook/LiveBudgetListForm.vb
@@ -22,8 +22,6 @@ Friend Class LiveBudgetListForm
 
     Private Sub cmdSearch_Click(ByVal eventSender As System.Object, ByVal eventArgs As System.EventArgs) Handles cmdSearch.Click
         Dim datTarget As Date
-        Dim lngIndex As Integer
-        Dim objTrx As Trx
         Dim objItem As System.Windows.Forms.ListViewItem
 
         Try
@@ -35,46 +33,43 @@ Friend Class LiveBudgetListForm
             datTarget = CDate(txtTargetDate.Text)
 
             lvwMatches.Items.Clear()
-            For lngIndex = 1 To mobjReg.lngTrxCount
-                objTrx = mobjReg.objTrx(lngIndex)
-                If objTrx.GetType() Is GetType(BudgetTrx) Then
-                    With DirectCast(objTrx, BudgetTrx)
-                        If .InBudgetPeriod(datTarget) Then
-                            objItem = UITools.ListViewAdd(lvwMatches)
-                            objItem.Text = Utilities.strFormatDate(.datDate)
+            For Each objTrx As BudgetTrx In mobjReg.colAllTrx(Of BudgetTrx)()
+                With objTrx
+                    If .InBudgetPeriod(datTarget) Then
+                        objItem = UITools.ListViewAdd(lvwMatches)
+                        objItem.Text = Utilities.strFormatDate(.datDate)
 
-                            If objItem.SubItems.Count > 1 Then
-                                objItem.SubItems(1).Text = .strDescription
-                            Else
-                                objItem.SubItems.Insert(1, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, .strDescription))
-                            End If
-
-                            If objItem.SubItems.Count > 2 Then
-                                objItem.SubItems(2).Text = mobjBudgets.strKeyToValue1(.strBudgetKey)
-                            Else
-                                objItem.SubItems.Insert(2, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, mobjBudgets.strKeyToValue1(.strBudgetKey)))
-                            End If
-
-                            If objItem.SubItems.Count > 3 Then
-                                objItem.SubItems(3).Text = Utilities.strFormatCurrency(.curBudgetLimit)
-                            Else
-                                objItem.SubItems.Insert(3, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, Utilities.strFormatCurrency(.curBudgetLimit)))
-                            End If
-
-                            If objItem.SubItems.Count > 4 Then
-                                objItem.SubItems(4).Text = Utilities.strFormatCurrency(.curBudgetApplied)
-                            Else
-                                objItem.SubItems.Insert(4, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, Utilities.strFormatCurrency(.curBudgetApplied)))
-                            End If
-
-                            If objItem.SubItems.Count > 5 Then
-                                objItem.SubItems(5).Text = Utilities.strFormatCurrency(.curAmount)
-                            Else
-                                objItem.SubItems.Insert(5, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, Utilities.strFormatCurrency(.curAmount)))
-                            End If
+                        If objItem.SubItems.Count > 1 Then
+                            objItem.SubItems(1).Text = .strDescription
+                        Else
+                            objItem.SubItems.Insert(1, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, .strDescription))
                         End If
-                    End With
-                End If
+
+                        If objItem.SubItems.Count > 2 Then
+                            objItem.SubItems(2).Text = mobjBudgets.strKeyToValue1(.strBudgetKey)
+                        Else
+                            objItem.SubItems.Insert(2, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, mobjBudgets.strKeyToValue1(.strBudgetKey)))
+                        End If
+
+                        If objItem.SubItems.Count > 3 Then
+                            objItem.SubItems(3).Text = Utilities.strFormatCurrency(.curBudgetLimit)
+                        Else
+                            objItem.SubItems.Insert(3, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, Utilities.strFormatCurrency(.curBudgetLimit)))
+                        End If
+
+                        If objItem.SubItems.Count > 4 Then
+                            objItem.SubItems(4).Text = Utilities.strFormatCurrency(.curBudgetApplied)
+                        Else
+                            objItem.SubItems.Insert(4, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, Utilities.strFormatCurrency(.curBudgetApplied)))
+                        End If
+
+                        If objItem.SubItems.Count > 5 Then
+                            objItem.SubItems(5).Text = Utilities.strFormatCurrency(.curAmount)
+                        Else
+                            objItem.SubItems.Insert(5, New System.Windows.Forms.ListViewItem.ListViewSubItem(Nothing, Utilities.strFormatCurrency(.curAmount)))
+                        End If
+                    End If
+                End With
             Next
 
             Exit Sub
--- PersistTools/RegisterSaver.vb
+++ PersistTools/RegisterSaver.vb
@@ -15,14 +15,11 @@ Public Class RegisterSaver
     Private mobjSaveFile As StreamWriter
 
     Public Sub Save(ByVal objSaveFile_ As StreamWriter, ByVal objReg_ As Register, ByVal colFakeLines_ As ICollection(Of String))
-
-        Dim lngIndex As Integer
-
         mobjSaveFile = objSaveFile_
         mobjReg = objReg_
         mcolFakeLines = colFakeLines_
-        For lngIndex = 1 To objReg_.lngTrxCount
-            SaveTrx(objReg_.objTrx(lngIndex))
+        For Each objTrx As Trx In objReg_.colAllTrx(Of Trx)()
+            SaveTrx(objTrx)
         Next
     End Sub
 
