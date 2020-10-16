7558dc7f4d8c51d5737b0ba1e80e145fa32c0f1b
Timothy Yaukey <yaukey@willowsoft.com>
2020-10-15 20:36:36 -0700

    Add close-checkbookcompany, new-checkbooknormaltrx, and Test1.ps1.

---
+++ Powershell/CloseCompany.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+using Willowsoft.CheckBook.Lib;
+using Willowsoft.CheckBook.PersistTools;
+
+namespace Willowsoft.CheckBook.Powershell
+{
+    [Cmdlet(VerbsCommon.Close, "CheckbookCompany")]
+    [OutputType(typeof(Company))]
+    public class CloseCompany : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Company Company { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            CompanySaver.SaveChangedAccounts(Company);
+            Company.UnlockData();
+        }
+    }
+}
---
+++ Powershell/Test1.ps1
@@ -0,0 +1,9 @@
+﻿import-module .\bin\debug\Willowsoft.Checkbook.Powershell.dll
+$cmp = open-checkbookcompany -path "c:\programdata\Willow Creek Checkbook\Tim Test Company"
+$cmp.colAccounts
+$acct = get-checkbookaccount -company $cmp -accountname "Checking Account"
+$reg = get-checkbookregister -account $acct -registername "Checking Account"
+$reg
+$trx = new-checkbooknormaltrx -register $reg -date 2/10/2019 -number Card -description "Amazon Warehouse" -status Reconciled
+$trx
+close-checkbookcompany -company $cmp
