b98daee475ef79c229e48bb6fa4028047b8b6899
Timothy Yaukey <yaukey@willowsoft.com>
2020-10-17 19:51:37 -0700

    try/finally in Test1.ps1.

--- Powershell/Test1.ps1
+++ Powershell/Test1.ps1
@@ -1,9 +1,13 @@
 ﻿import-module .\bin\debug\Willowsoft.Checkbook.Powershell.dll
 $cmp = open-checkbookcompany -path "c:\programdata\Willow Creek Checkbook\Tim Test Company"
-$cmp.colAccounts
+try
+{
 $acct = get-checkbookaccount -company $cmp -accountname "Checking Account"
 $reg = get-checkbookregister -account $acct -registername "Checking Account"
-$reg
 $trx = new-checkbooknormaltrx -register $reg -date 2/10/2019 -number Card -description "Amazon Warehouse" -status Reconciled
 $trx
+}
+finally
+{
 close-checkbookcompany -company $cmp
+}
