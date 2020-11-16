afd7d1a82ddcd49efe5cdfdf206e57f9f6c20c77
Timothy Yaukey <yaukey@willowsoft.com>
2020-11-15 20:24:09 -0800

    Test more Powershell features in Test1.ps1.

--- Powershell/Test1.ps1
+++ Powershell/Test1.ps1
@@ -7,7 +7,7 @@ $reg = get-checkbookregister -account $acct -registername "Checking Account"
 
 get-checkbooktrx -register $reg -startdate 2/1/2019 -enddate 2/28/2019|remove-checkbooktrx
 
-$split = new-checkbooksplit -company $cmp -category E:Groceries -amount -100.00
+$split = new-checkbooksplit -company $cmp -category E:Groceries -amount -100.00 -invoicenum "IN1001" -invoicedate 2/5/2019 -duedate 3/1/2019 -memo yesdoit -ponumber PO4000
 add-checkbooknormaltrx -register $reg -date 2/10/2019 -number Card -description "Amazon Warehouse" -status Unreconciled -fake -OneSplit $split
 
 $split = new-checkbooksplit -company $cmp -category A:Savings -amount 500.00
@@ -15,6 +15,10 @@ add-checkbooknormaltrx -register $reg -date 2/10/2019 -number Dep -description "
 
 $split = new-checkbooksplit -company $cmp -category E:Groceries -budget Groceries -amount -20.05
 add-checkbooknormaltrx -register $reg -date 2/11/2019 -number Card -description "Thriftway" -status Unreconciled -memo "short trip" -OneSplit $split
+
+$split = new-checkbooksplit -company $cmp -category E:Groceries -amount -34.95
+$split2 = new-checkbooksplit -company $cmp -category E:Clothing -amount -194.500
+add-checkbooknormaltrx -register $reg -date 2/11/2019 -number 20004 -description "Fred Meyer" -status Reconciled -Splits ($split, $split2)
 
 get-checkbooktrx -register $reg -startdate 2/1/2019 -enddate 2/28/2019|get-checkbooksimpletrx|format-table
 }
